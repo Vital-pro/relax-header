15e3fe45b8e3d52aa3daccca7f9b07b8ea063581
Vital-pro <vitupikov@yandex.ru>
2024-03-20 01:56:08 +0300

    new project

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+      style: `
+      --move-x: ${(e.clientX - window.innerWidth/2) * -.007}deg;
+      --move-y: ${(e.clientY - window.innerHeight/2) * -.018}deg;
+      
+      `
+    })
+  })
+})
